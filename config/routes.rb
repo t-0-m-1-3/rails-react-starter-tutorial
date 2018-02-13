93b95627692b3035c7dfa93c32935a43f98d228b
tom goodheart <20537914+t-0-m-1-3@users.noreply.github.com>
2018-02-13 17:29:21 -0500

    moved to psql

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  # get 'pages/home'
-    root to: "pages#home"
-    namespace :api, defaults: {format: :json} do
-      resources :quotes, only: [ :show ]
-            
-        end
-    end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#
-Quote.delete_all
-
-Quote.create! (
-  [
-    {
-      text: "Be yourself; everyone else is already taken.",
-      author: "Oscar Wilde"
-    },
-    {
-      text: "Two things are infinite: the universe and human stupidity; " \
-            "and I'm not sure about the universe.",
-      author: "Albert Einstein"
-    },
-    {
-      text: "So many books, so little time.",
-      author: "Frank Zappa"
-    },
-    {
-      text: "Be the change that you wish to see in the world",
-      author: "Mahatma Gandhi"
-    },
-    {
-      text: "If you tell the truth, you don't have to remember anything.",
-      author: "Mark Twain"
-    }
-  ]
-)
-puts " #{Quote.count}  Quotes seeded!"
